23902b82c502f272506d3715e53b84a79eeda1b3
Alwin Varghese <alwin.varghese02@gmail.com>
2024-09-03 15:10:28 +0100

    Building Navbar Component

--- moviemaniac/src/App.jsx
+++ moviemaniac/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './App.css'
+import Navbar from "./components/Navbar/Navbar"
 const App = () => {
   return (
     <div className = 'app'>
-      <nav className='navbar'>Navbar content</nav>
+      <Navbar/>
       <main>Main Content</main>
     </div>
   )
