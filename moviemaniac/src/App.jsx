86c2ff3942f952773fe89153eba4d853b4d898d1
Alwin Varghese <alwin.varghese02@gmail.com>
2024-09-04 03:35:02 +0100

    Developing and styling the MovieList component

--- moviemaniac/src/App.jsx
+++ moviemaniac/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
 import Navbar from "./components/Navbar/Navbar"
+import MovieList from "./components/MovieList/movieList"
 const App = () => {
   return (
     <div className = 'app'>
       <Navbar/>
-      <main>Main Content</main>
+      <main>
+        <MovieList />
+      </main>
     </div>
   )
 }
--- moviemaniac/src/components/Navbar/Navbar.jsx
+++ moviemaniac/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import "./Navbar.css"
+import "./navbar.css"
 import Fire from '../../assets/fire.png'
 import Star from '../../assets/star.png'
 import Party from '../../assets/party.png'
